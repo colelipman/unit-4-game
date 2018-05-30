bc544157d3a88f20c10aaa3f3035618ce96a7d78
Clips <lipmancole@gmail.com>
2018-05-30 17:33:18 -0400

    images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
-	crystals = ['assets/images/red.png','assets/images/blue.png','assets/images/yellow.png','assets/images/green.png'];
+	crystals = ['assets/images/red.png',
+				'assets/images/blue.png',
+				'assets/images/yellow.png',
+				'assets/images/green.png'
+			];
 
 	var counter = 0;
 	var wins = 0;
